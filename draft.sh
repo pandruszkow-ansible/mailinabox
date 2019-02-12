7d20f92ffeed769cb9abea3de55d8fb213b98a4c
Piotr Andruszkow <pandruszkow@zoho.com>
2019-02-12 20:08:27 +0000

    Update draft.sh

--- draft.sh
+++ draft.sh
@@ -1,6 +1,10 @@
+#set custom data store path
 export STORAGE_ROOT=/srv/mail
+# install
 curl -s https://mailinabox.email/setup.sh | sudo -E bash
+# enable pinging for updates
 echo "privacy: false" > "$STORAGE_ROOT/settings.yaml"
+# enable redirect to mail when landing on / root
 echo <<EOF > "$STORAGE_ROOT/www/default/index.html"
 <html>
         <head>

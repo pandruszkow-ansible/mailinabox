546acfd9d0813a4174fc3add39ece1f3e4267ea9
Piotr Andruszkow <pandruszkow@zoho.com>
2019-02-12 20:07:19 +0000

    Update draft.sh

--- draft.sh
+++ draft.sh
@@ -1,3 +1,12 @@
 export STORAGE_ROOT=/srv/mail
 curl -s https://mailinabox.email/setup.sh | sudo -E bash
 echo "privacy: false" > "$STORAGE_ROOT/settings.yaml"
+echo <<EOF > "$STORAGE_ROOT/www/default/index.html"
+<html>
+        <head>
+                <title>Redirecting...</title>
+                <meta http-equiv="refresh" content="0; url='https://box.andruszkow.com/mail/" />
+        </head>
+        <body />
+</html>
+EOF
